673981d89d92b8d243fda78d356722715bb49a9f
Kelsey Guffanti <kc.breen3@gmail.com>
2023-07-09 09:45:04 -0400

    tree class methods completed with passed tests

--- tree.js
+++ tree.js
@@ -15,20 +15,47 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    let sum = 0;
+    if (this.root === null) return sum;
+
+    let vals = [this.root];
+    while (vals.length) {
+      let current = vals.pop();
+      sum += current.val;
+      vals = [...vals, ...current.children];
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    let count = 0;
+    if (this.root === null) return count;
 
+    let nodes = [this.root];
+    while (nodes.length) {
+      let current = nodes.pop();
+      if (current.val % 2 == 0) count += 1;
+      nodes = [...nodes, ...current.children];
+    }
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    let count = 0;
+    if (this.root === null) return count;
 
+    let nodes = [this.root];
+    while (nodes.length) {
+      let current = nodes.pop();
+      if (current.val > lowerBound) count += 1;
+      nodes = [...nodes, ...current.children];
+    }
+    return count;
   }
 }
 
